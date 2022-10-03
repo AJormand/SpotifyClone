956b71114f91cd3cbb7aede18059b622342dc92a
AJormand <83412039+AJormand@users.noreply.github.com>
2022-10-03 20:24:43 +0200

    around you page added

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -9,6 +9,9 @@ const AroundYou = () => {
   const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery("DE");
+
+  // Get by country doesnt work for Slovenia --> "msg":"value is not a valid enumeration member; permitted: 'DE', 'PT', 'DK', 'HR', 'UA', 'HU', 'MA', 'ID', 'IE', 'US', 'EG', 'IL', 'UY', 'IN', 'ZA', 'IT', 'MX', 'MY', 'ES', 'VE', 'AR', 'AT', 'AU', 'RO', 'NL', 'NO', 'RU', 'BE', 'FI', 'BG', 'JP', 'FR', 'NZ', 'SA', 'BR', 'SE', 'SG', 'BY', 'GB', 'CA', 'CH', 'KR', 'CL', 'GR', 'CN', 'CO', 'KZ', 'CR', 'TH', 'PE', 'CZ', 'PL', 'TR'","type":"type_error.enum","ctx":{"enum_values":["DE","PT","DK","HR","UA","HU","MA","ID","IE","US","EG","IL","UY","IN","ZA","IT","MX","MY","ES","VE","AR","AT","AU","RO","NL","NO","RU","BE","FI","BG","JP","FR","NZ","SA","BR","SE","SG","BY","GB","CA","CH","KR","CL","GR","CN","CO","KZ","CR","TH","PE","CZ","PL","TR"
 
   console.log(country);
 
@@ -22,7 +25,31 @@ const AroundYou = () => {
       .finally(() => setLoading(false));
   }, [country]);
 
-  return <div></div>;
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country !== "") return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around You:
+        <span className="font-black"> {country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AroundYou;
